98e54a6c8faf7345f2813d55602951dd67a72def
Syed Usman Ali <urusman543@gmail.com>
2024-04-27 11:57:58 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hotel-Management-System
+ "🏨 Manage with ease: Hotel Management System crafted in ASP.NET, HTML, CSS, and JS, backed by robust database SQL for seamless operations!
---
+++ db project/popup.js
@@ -0,0 +1,8 @@
+function openpopup(){
+    let popup=document.getElementById("popup");
+    popup.classList.add("open-popup");
+}
+function closepopup(){
+    let popup=document.getElementById("popup");
+    popup.classList.remove("open-popup");
+}
